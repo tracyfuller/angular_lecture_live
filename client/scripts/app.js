a8d8b0b55c69a7cefdcd9e1225f4aeda1d1961d9
tracyfuller <tracy.e.fuller@gmail.com>
2015-05-18 17:06:57 -0500

    added change language button, fixed i looping functionality to tie to the change language button, now displaying language

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,40 +1,41 @@
 var myApp = angular.module('myApp',[]); //this is a standard way to declare a module! names match, empty brackets
 
 var greetingArray = [
-    ["Hello ", "English"],
-    ["Bonjour ", "French"],
-    ["Hola ", "Spanish"],
-    ["Pozdrav ", "Croatian"],
-    ["Hallo ", "Dutch"],
-    ["Kumusta ", "Filipino"],
-    ["Bonjou ", "Haitian"],
-    ["Sveiki ", "Lithuanian"],
-    ["Hei ", "Norwegian"],
-    ["Habari ", "Swahili"],
-    ["Hej ", "Swedish"],
-    ["Helo ", "Welsh"],
-    ["Sawubona ", "Zulu"]
+    ["Hello ", "Goodbye ", "English"],
+    ["Bonjour ", "Adieu ", "French"],
+    ["Hola ", "Adiós ", "Spanish"],
+    ["Pozdrav ", "Zbogom ", "Croatian"],
+    ["Hallo ", "afscheid ", "Dutch"],
+    ["Kumusta ", "Paalam ", "Filipino"],
+    ["Bonjou ", "Adye ", "Haitian"],
+    ["Sveiki ", "Atsisveikinimas ", "Lithuanian"],
+    ["Hei ", "farvel ", "Norwegian"],
+    ["Habari ", "kuaga ", "Swahili"],
+    ["Hej ", "adjö ", "Swedish"],
+    ["Helo ", "Hwyl fawr ", "Welsh"],
+    ["Sawubona ", "Hamba kahle", "Zulu"]
 ];
 var i = 0
 
 myApp.controller('WelcomeController',["$scope", function($scope){
     $scope.heading = "Message: ";
+    $scope.language = greetingArray[i][2];
+
+    $scope.changeLanguage = function(){
+        i++;
+        if (i >= greetingArray.length){
+            i = 0;
+        };
+        $scope.language = greetingArray[i][2];
+    }
 
     $scope.updateMessage = function(){
         $scope.message = greetingArray[i][0] + $scope.name + "!";
         console.log("Greeting array: ", greetingArray[i][0]);
-        i++;
-        //while (i < greetingArray.length){
-        //    if (i >= greetingArray.length){
-        //        i = 0;
-        //    };
-        //    $scope.message = greetingArray[i][0] + $scope.name + "!";
-        //    i++;
-        //
-        //}
+
     };
 
     $scope.goodByeMessage = function(){
-        $scope.message = "Goodbye, " + $scope.name + " :(";
+        $scope.message = greetingArray[i][1] + $scope.name + " :(";
     };
 }]);
