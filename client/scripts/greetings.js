9a41356b117f8fd0d6b79f0d23584b5265afb91b
tracyfuller <tracy.e.fuller@gmail.com>
2015-05-19 16:06:15 -0500

    Not sure what's changed.

--- client/scripts/greetings.js
+++ client/scripts/greetings.js
@@ -23,21 +23,4 @@ angular.module('myApp', [])
     };
 });
 
-//var greetingArray = [
-//    ["Hello ", "Goodbye ", "English"],
-//    ["Bonjour ", "Adieu ", "French"],
-//    ["Hola ", "Adiós ", "Spanish"],
-//    ["Pozdrav ", "Zbogom ", "Croatian"],
-//    ["Hallo ", "afscheid ", "Dutch"],
-//    ["Kumusta ", "Paalam ", "Filipino"],
-//    ["Bonjou ", "Adye ", "Haitian"],
-//    ["Sveiki ", "Atsisveikinimas ", "Lithuanian"],
-//    ["Hei ", "farvel ", "Norwegian"],
-//    ["Habari ", "kuaga ", "Swahili"],
-//    ["Hej ", "adjö ", "Swedish"],
-//    ["Helo ", "Hwyl fawr ", "Welsh"],
-//    ["Sawubona ", "Hamba kahle ", "Zulu"]
-//];
-
-//module.exports = greetingArray;
 
